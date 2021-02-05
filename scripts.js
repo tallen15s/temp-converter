28e4b87d3fc24b386039e158b386134362ddcbd8
Tom Allen <tomallen851@yahoo.com>
2021-02-05 15:59:01 -0600

    Made changes to lib, scripts and index for converter

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// when you click and start typing in either input,
+// update the others input value with the converted temps
+//  1 "keyup events"
+// 2 use function, keyword so that you can use this
