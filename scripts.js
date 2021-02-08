7fd24518a8d4b4eb29c65ce21a0d2de81303c29e
Tom Allen <tomallen851@yahoo.com>
2021-02-08 16:34:53 -0600

    Add CSS styling add code to scripts added buttons in Index

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@
 // update the others input value with the converted temps
 //  1 "keyup events"
 // 2 use function, keyword so that you can use this
+// eslint-disable-next-line no-unused-expressions
+import converter from "./lib.js";
+
+const oldTemp = document.querySelector("input");
+
+oldTemp.addEventListener("keyup", (e) => {
+  const newTemp = converter(oldTemp.value, "toCelsius");
+  console.log(newTemp);
+});
